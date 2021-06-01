34d2def4940d056436eb908a2bdd473f7ca4222c
王嬿甯 Judy Wang <judynining@gmail.com>
2021-06-01 11:54:18 +0800

    Add files via upload

---
+++ 21點/21點函數架構.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+
+using namespace std;
+
+int game_satus = 0; // 0為尚未分出勝負   1為玩家勝  2為電腦勝 
+
+
+int DrawCard();
+void play();
+
+int main(){
+	
+	return 0;
+}
